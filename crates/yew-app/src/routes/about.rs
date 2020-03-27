5693b6ca2cbd181b30771f4c4593269d95df0ade
Jet Li <jing.i.qin@icloud.com>
2020-03-28 00:14:07 +0800

    Add tests for about page

--- crates/yew-app/src/routes/about.rs
+++ crates/yew-app/src/routes/about.rs
@@ -38,3 +38,26 @@ impl Component for About {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use wasm_bindgen_test::*;
+
+    wasm_bindgen_test_configure!(run_in_browser);
+
+    use super::About;
+    use yew::App;
+
+    #[wasm_bindgen_test]
+    fn about_page_has_an_app_link() {
+        let app: App<About> = yew::App::new();
+        app.mount(yew::utils::document().get_element_by_id("output").unwrap());
+
+        let app_links = yew::utils::document().get_elements_by_class_name("app-link");
+
+        assert_eq!(app_links.length(), 1);
+
+        let link = app_links.item(0).expect("No app-link").inner_html();
+        assert_eq!(link, "Create Yew App");
+    }
+}
