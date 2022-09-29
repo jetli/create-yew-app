56fd4a3287632d3cd59cac0e7748dcae93230a9a
Orlando Vargas <nojipiz@protonmail.com>
2022-09-29 14:50:01 -0500

    Replace unmaintained "wee_alloc" to std::alloc

--- crates/yew-app/src/lib.rs
+++ crates/yew-app/src/lib.rs
@@ -6,9 +6,9 @@ use wasm_bindgen::prelude::*;
 
 use app::App;
 
-// Use `wee_alloc` as the global allocator.
+// Use `std::alloc` as the global allocator.
 #[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+static ALLOC: std::alloc::System = std::alloc::System;
 
 // This is the entry point for the web app
 #[wasm_bindgen]
